c4f3084366d18639cf8ce37a4fe80d0f6fa115fa
sarojoffl <sarojoffl@gmail.com>
2025-06-27 11:19:25 +0545

    refactor: update Consular Services page and navigation menu

--- main/urls.py
+++ main/urls.py
@@ -5,10 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
-    path('visa-info/', views.visa_info, name='visa_info'),
-    path('apply-form/', views.apply_form, name='apply_form'),
     path('useful-link/', views.useful_link, name='useful_link'),
     path('articles/', views.articles, name='articles'),
-    path('gallery/', views.gallery, name='gallery'),
     path('contact/', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -9,20 +9,11 @@ def about(request):
 def services(request):
     return render(request, 'main/services.html')
 
-def visa_info(request):
-    return render(request, 'main/visa_info.html')
-
-def apply_form(request):
-    return render(request, 'main/apply_form.html')
-
 def useful_link(request):
     return render(request, 'main/useful_link.html')
 
 def articles(request):
     return render(request, 'main/articles.html')
-
-def gallery(request):
-    return render(request, 'main/gallery.html')
 
 def contact(request):
     return render(request, 'main/contact.html')
